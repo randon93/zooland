2adca1231db898e426614f6f0c4efec39297d00a
randon93 <32597818+randon93@users.noreply.github.com>
2019-06-09 17:47:36 -0500

    brandon

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once ("CONFIG/config.php");
+require_once ("LIB/conexion.php");
+require_once ("LIB/controlador.php");
+require_once ("LIB/modelo.php");
+require_once ("LIB/router.php");
+require_once ("LIB/vista.php");
+require_once ("LIB/sesion.php");
+
+$app = new Router();
+ ?>
