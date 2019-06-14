6fe5e79b7abe961c73ad0ae62625131dba8e27cc
randon93 <32597818+randon93@users.noreply.github.com>
2019-06-13 21:01:54 -0500

    Update sesion.php

--- LIB/sesion.php
+++ LIB/sesion.php
@@ -1,19 +1,24 @@
-<?php /**
+<?php
+/**
  *
  */
-class Sesion  {
+class Sesion {
+
+  private $user ;
 
   function __construct()  {
-    session_start();
+      session_start();
   }
-  
+
   public function setCurrentUser($usuariOn){
     $_SESSION['USER'] = $usuariOn;
     $this->user = $_SESSION['USER'];
   }
+
   public function getCurrentUser(){
     return $_SESSION['USER'];
   }
+
   public function cerrarSesion(){
     session_unset();
     session_destroy();
